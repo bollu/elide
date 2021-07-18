f4d591c5ac3a375376609f8b66214de3e24e4514
Siddharth Bhat <siddu.druid@gmail.com>
2021-07-18 20:11:00 +0530

    go through ch2 of kilo editor

---
+++ README.md
@@ -0,0 +1,15 @@
+# txt
+- `TXT` suX at Text editing
+- minimal C++ IDE written for myself by myself.
+- vim keybindings.
+
+
+#### Building
+
+```cpp
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -GNinja ../
+```
+
+#### References
+- [KILO: build your own text editor](https://viewsourcecode.org/snaptoken/kilo/)
+- [vis editor](https://github.com/martanne/vis)
---
+++ main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <termios.h>
+#include <unistd.h>
+
+
+/*** data ***/
+
+struct termios orig_termios;
+
+/*** terminal ***/
+
+
+void die(const char *s) {
+  // explain errno before dying with mesasge s.
+  perror(s);
+  exit(1);
+}
+
+void disableRawMode() {
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+    die("tcsetattr");
+  }
+}
+
+void enableRawMode() {
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+    die("tcgetattr");
+  };
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
+  int lflags = 0;
+  lflags |= ECHO;   // don't echo back.
+  lflags |= ICANON; // read byte at a time.
+  lflags |= ISIG;   // no C-c, C-z for me!
+  lflags |= IEXTEN; // Disables C-v for waiting for another character to be sent
+                    // literally.
+  raw.c_lflag &= ~(lflags);
+
+  // | disable flow control. C-s/C-q to stop sending and start sending data.
+  int iflags = 0;
+  iflags |= (IXON);
+  // disable translating C-m (\r) into \n. (Input-Carriage-Return-NewLine)
+  iflags |= ICRNL;
+  // INPCK enables parity checking, which doesn’t seem to apply to modern
+  // terminal emulators. ISTRIP causes the 8th bit of each input byte to be
+  // stripped, meaning it will set it to 0. This is probably already turned off.
+  // When BRKINT is turned on, a break condition will cause a SIGINT signal to
+  // be sent to the program, like pressing Ctrl-C.
+  iflags |= BRKINT | INPCK | ISTRIP;
+  raw.c_iflag &= ~iflags;
+
+  // disable all output processing of output.
+  raw.c_oflag &= ~(OPOST);
+
+  raw.c_cflag |= (CS8); // This specifies eight bits per byte.
+
+  // | min # of bytes to be read before read() return
+  raw.c_cc[VMIN] = 0;
+  // | max amount of time to wait before read() return.
+  raw.c_cc[VTIME] = 1;
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+    die("tcsetattr");
+  }
+}
+
+/*** init ***/
+
+
+int main() {
+  enableRawMode();
+  while (1) {
+    char c = 0;
+
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+      die("read");
+    };
+
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == 'q')
+      break;
+  };
+
+  return 0;
+}
