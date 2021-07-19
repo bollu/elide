08f8caa48ff191392f4ef5fe398a1f6b4a696dc0
Siddharth Bhat <siddu.druid@gmail.com>
2021-07-19 16:22:36 +0530

    user should not scroll of horizontal

--- main.cpp
+++ main.cpp
@@ -390,6 +390,8 @@ void editorRefreshScreen() {
 /*** input ***/
 
 void editorMoveCursor(int key) {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
   switch (key) {
   case ARROW_LEFT:
     if (E.cx != 0) {
@@ -397,9 +399,9 @@ void editorMoveCursor(int key) {
     }
     break;
   case ARROW_RIGHT:
-    // if (E.cx != E.screencols - 1) {
+    if (row && E.cx < row->size) {
       E.cx++;
-    // }
+    }
     break;
   case ARROW_UP:
     if (E.cy != 0) {
