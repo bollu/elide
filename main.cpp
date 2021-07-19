868d4fb82e2a053dd32eb1e0abfeaf8659c727d0
Siddharth Bhat <siddu.druid@gmail.com>
2021-07-19 16:24:44 +0530

    add move left for prev line

--- main.cpp
+++ main.cpp
@@ -396,7 +396,13 @@ void editorMoveCursor(int key) {
   case ARROW_LEFT:
     if (E.cx != 0) {
       E.cx--;
+    } else if (E.cy > 0) {
+      // move back line.
+      assert(E.cx == 0);
+      E.cy--;
+      E.cx = E.row[E.cy].size;
     }
+
     break;
   case ARROW_RIGHT:
     if (row && E.cx < row->size) {
@@ -414,12 +420,18 @@ void editorMoveCursor(int key) {
     }
     break;
   case PAGE_DOWN:
-    E.cy = std::min<int>(E.cy + E.screenrows/2, E.numrows);
+    E.cy = std::min<int>(E.cy + E.screenrows / 2, E.numrows);
     break;
   case PAGE_UP:
-    E.cy = std::max<int>(E.cy - E.screenrows/2, 0);
+    E.cy = std::max<int>(E.cy - E.screenrows / 2, 0);
     break;
+  }
 
+  // snap to next line.
+  row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row->size : 0;
+  if (E.cx > rowlen) {
+    E.cx = rowlen;
   }
 }
 
